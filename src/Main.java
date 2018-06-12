8f2402a0de6ddc99ffc5133f79b3905cfd9ed326
kirill-memphis <memphis09@list.ru>
2018-06-12 14:47:52 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    static int intCount;
+    static int Number;
+    static int[] arrNumber;
+
+    public static void main(String[] args) {
+
+        File file = new File("src/arr.txt");
+        try {
+            Scanner countScanner = new Scanner(file);
+
+            intCount = 0;
+
+            while (countScanner.hasNextLine()) {
+
+                //Считаем количество значений в файле
+                int line = countScanner.nextInt();
+                //System.out.println(line);
+
+                intCount++;
+            }
+            countScanner.close();
+
+            //Проверка подсчета значений
+            System.out.println("Значений: "+intCount);
+
+            //Инициализация массива
+            Scanner scanner = new Scanner(file);
+            arrNumber = new int[intCount];
+            for (int i=0; i<intCount; i++) {
+
+                int newNumber = scanner.nextInt();
+                arrNumber[i] = newNumber;
+            }
+
+            for (int j=0; j<intCount; j++)
+                System.out.println(arrNumber[j]);
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+            //Сортировка отрицательных в начало
+            int belowCounter = 0;
+            int[] arrBelowSort = new int[intCount];
+
+            for (int k=0; k<intCount; k++) {
+
+                if (arrNumber[k]<0){
+                    arrBelowSort[belowCounter]=arrNumber[k];
+                    belowCounter++;
+                }
+
+            }
+            System.out.println("----------");
+
+
+            for (int l=0; l<intCount; l++)
+                System.out.print(arrBelowSort[l]);
+
+
+        }
+
+    }
+
